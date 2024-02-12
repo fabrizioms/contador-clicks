f0e930e482bdd478f30eb083fc25413b4349667f
fabrizio <fabriziomundaca@gmail.com>
2024-02-12 11:28:15 -0500

    clickcounter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/Button";
+import Contador from "./components/Contador";
+import fabdevlogo from './images/fm-circle.png';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const[numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  }
+
+  const reiniciarContador = () =>  {
+    setNumClicks(0);
+    
+  }
+
+  return <div className="App">
+    <div className="logo-contenedor">
+      <img className="logo"
+      src={fabdevlogo}
+      alt="mi logo"
+      />
     </div>
-  );
+    <div className="contador-contenedor">
+      <Contador
+        numClicks={numClicks}
+      />
+      <Button
+        texto="Click"
+        esBotonDeClick={true}
+        manejarClick={manejarClick}
+        />
+      <Button
+        texto="Reiniciar"
+        esBotonDeClick={false}
+        manejarClick={reiniciarContador}
+        />
+    </div>
+  </div>;
 }
 
 export default App;
